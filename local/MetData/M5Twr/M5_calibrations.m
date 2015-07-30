aead11ed176bdb1f86f758b504128e907622ce09
Andy Clifton <andy_clifton@yahoo.com>
2015-07-29 18:21:38 -0600

    User friendly structure
    Added example data and file structure

---
+++ local/MetData/M5Twr/M5_calibrations.m
@@ -0,0 +1,231 @@
+%
+calibrations{1}.channel = 65;
+calibrations{1}.startdatenum = datenum([2015 01 01 00 00 00]) ;
+calibrations{1}.stopdatenum = datenum([2015 07 06 17 10 00]) ;
+calibrations{1}.reason = 'Converting to TDMS system';
+calibrations{1}.dateadded = datenum([2015 07 15 15 34 00]);
+calibrations{1}.from.gradient = 0.046530;
+calibrations{1}.from.offset = 0.192610;
+calibrations{1}.to.gradient = 0.046550;
+calibrations{1}.to.offset = 0.190040;
+
+calibrations{2}.channel = 66;
+calibrations{2}.startdatenum = datenum([2015 01 01 00 00 00]) ;
+calibrations{2}.stopdatenum = datenum([2015 07 06 17 10 00]) ;
+calibrations{2}.reason = 'Converting to TDMS system';
+calibrations{2}.dateadded = datenum([2015 07 15 15 34 00]);
+calibrations{2}.from.gradient = 0.042980;
+calibrations{2}.from.offset = 0.295140;
+calibrations{2}.to.gradient = 0.043620;
+calibrations{2}.to.offset = 0.318921;
+
+calibrations{3}.channel = 67;
+calibrations{3}.startdatenum = datenum([2015 01 01 00 00 00]) ;
+calibrations{3}.stopdatenum = datenum([2015 07 06 17 10 00]) ;
+calibrations{3}.reason = 'Converting to TDMS system';
+calibrations{3}.dateadded = datenum([2015 07 15 15 34 00]);
+calibrations{3}.from.gradient = 0.046550;
+calibrations{3}.from.offset = 0.195170;
+calibrations{3}.to.gradient = 0.046620;
+calibrations{3}.to.offset = 0.175760;
+
+calibrations{4}.channel = 68;
+calibrations{4}.startdatenum = datenum([2015 01 01 00 00 00]) ;
+calibrations{4}.stopdatenum = datenum([2015 07 06 17 10 00]) ;
+calibrations{4}.reason = 'Converting to TDMS system';
+calibrations{4}.dateadded = datenum([2015 07 15 15 34 00]);
+calibrations{4}.from.gradient = 0.043100;
+calibrations{4}.from.offset = 0.327890;
+calibrations{4}.to.gradient = 0.043377;
+calibrations{4}.to.offset = 0.311678;
+
+calibrations{5}.channel = 69;
+calibrations{5}.startdatenum = datenum([2015 01 01 00 00 00]) ;
+calibrations{5}.stopdatenum = datenum([2015 07 06 17 10 00]) ;
+calibrations{5}.reason = 'Converting to TDMS system';
+calibrations{5}.dateadded = datenum([2015 07 15 15 34 00]);
+calibrations{5}.from.gradient = 0.046610;
+calibrations{5}.from.offset = 0.188510;
+calibrations{5}.to.gradient = 0.048350;
+calibrations{5}.to.offset = 0.240830;
+
+calibrations{6}.channel = 70;
+calibrations{6}.startdatenum = datenum([2015 01 01 00 00 00]) ;
+calibrations{6}.stopdatenum = datenum([2015 07 06 17 10 00]) ;
+calibrations{6}.reason = 'Converting to TDMS system';
+calibrations{6}.dateadded = datenum([2015 07 15 15 34 00]);
+calibrations{6}.from.gradient = 0.046600;
+calibrations{6}.from.offset = 0.178050;
+calibrations{6}.to.gradient = 0.048260;
+calibrations{6}.to.offset = 0.274270;
+
+calibrations{7}.channel = 71;
+calibrations{7}.startdatenum = datenum([2015 01 01 00 00 00]) ;
+calibrations{7}.stopdatenum = datenum([2015 07 06 17 10 00]) ;
+calibrations{7}.reason = 'Converting to TDMS system';
+calibrations{7}.dateadded = datenum([2015 07 15 15 34 00]);
+calibrations{7}.from.gradient = 0.042770;
+calibrations{7}.from.offset = 0.320720;
+calibrations{7}.to.gradient = 0.043785;
+calibrations{7}.to.offset = 0.313131;
+
+calibrations{8}.channel = 72;
+calibrations{8}.startdatenum = datenum([2015 01 01 00 00 00]) ;
+calibrations{8}.stopdatenum = datenum([2015 07 06 17 10 00]) ;
+calibrations{8}.reason = 'Converting to TDMS system';
+calibrations{8}.dateadded = datenum([2015 07 15 15 34 00]);
+calibrations{8}.from.gradient = 0.046640;
+calibrations{8}.from.offset = 0.169520;
+calibrations{8}.to.gradient = 0.048530;
+calibrations{8}.to.offset = 0.225140;
+
+calibrations{9}.channel = 73;
+calibrations{9}.startdatenum = datenum([2015 01 01 00 00 00]) ;
+calibrations{9}.stopdatenum = datenum([2015 07 06 17 10 00]) ;
+calibrations{9}.reason = 'Converting to TDMS system';
+calibrations{9}.dateadded = datenum([2015 07 15 15 34 00]);
+calibrations{9}.from.gradient = 0.042980;
+calibrations{9}.from.offset = 0.311870;
+calibrations{9}.to.gradient = 0.043760;
+calibrations{9}.to.offset = 0.283172;
+
+calibrations{10}.channel = 74;
+calibrations{10}.startdatenum = datenum([2015 01 01 00 00 00]) ;
+calibrations{10}.stopdatenum = datenum([2015 07 06 17 10 00]) ;
+calibrations{10}.reason = 'Converting to TDMS system';
+calibrations{10}.dateadded = datenum([2015 07 15 15 34 00]);
+calibrations{10}.from.gradient = 0.042990;
+calibrations{10}.from.offset = 0.290550;
+calibrations{10}.to.gradient = 0.043687;
+calibrations{10}.to.offset = 0.333586;
+
+calibrations{11}.channel = 30;
+calibrations{11}.startdatenum = datenum([2015 01 01 00 00 00]) ;
+calibrations{11}.stopdatenum = datenum([2015 07 06 17 10 00]) ;
+calibrations{11}.reason = 'Converting to TDMS system';
+calibrations{11}.dateadded = datenum([2015 07 15 15 34 00]);
+calibrations{11}.from.gradient = 19.997900;
+calibrations{11}.from.offset = -49.982506;
+calibrations{11}.to.gradient = 20.078750;
+calibrations{11}.to.offset = -50.220200;
+
+calibrations{12}.channel = 31;
+calibrations{12}.startdatenum = datenum([2015 01 01 00 00 00]) ;
+calibrations{12}.stopdatenum = datenum([2015 07 06 17 10 00]) ;
+calibrations{12}.reason = 'Converting to TDMS system';
+calibrations{12}.dateadded = datenum([2015 07 15 15 34 00]);
+calibrations{12}.from.gradient = 19.994300;
+calibrations{12}.from.offset = -49.974103;
+calibrations{12}.to.gradient = 20.077330;
+calibrations{12}.to.offset = -50.210600;
+
+calibrations{13}.channel = 32;
+calibrations{13}.startdatenum = datenum([2015 01 01 00 00 00]) ;
+calibrations{13}.stopdatenum = datenum([2015 07 06 17 10 00]) ;
+calibrations{13}.reason = 'Converting to TDMS system';
+calibrations{13}.dateadded = datenum([2015 07 15 15 34 00]);
+calibrations{13}.from.gradient = 19.984900;
+calibrations{13}.from.offset = -49.942180;
+calibrations{13}.to.gradient = 20.621400;
+calibrations{13}.to.offset = -50.169000;
+
+calibrations{14}.channel = 36;
+calibrations{14}.startdatenum = datenum([2015 01 01 00 00 00]) ;
+calibrations{14}.stopdatenum = datenum([2015 07 06 17 10 00]) ;
+calibrations{14}.reason = 'Converting to TDMS system';
+calibrations{14}.dateadded = datenum([2015 07 15 15 34 00]);
+calibrations{14}.from.gradient = 19.999500;
+calibrations{14}.from.offset = -50.025020;
+calibrations{14}.to.gradient = 20.454900;
+calibrations{14}.to.offset = -41.181400;
+
+calibrations{15}.channel = 37;
+calibrations{15}.startdatenum = datenum([2015 01 01 00 00 00]) ;
+calibrations{15}.stopdatenum = datenum([2015 07 06 17 10 00]) ;
+calibrations{15}.reason = 'Converting to TDMS system';
+calibrations{15}.dateadded = datenum([2015 07 15 15 34 00]);
+calibrations{15}.from.gradient = 2.217900;
+calibrations{15}.from.offset = -4.434682;
+calibrations{15}.to.gradient = 2.238506;
+calibrations{15}.to.offset = -4.475070;
+
+calibrations{16}.channel = 38;
+calibrations{16}.startdatenum = datenum([2015 01 01 00 00 00]) ;
+calibrations{16}.stopdatenum = datenum([2015 07 06 17 10 00]) ;
+calibrations{16}.reason = 'Converting to TDMS system';
+calibrations{16}.dateadded = datenum([2015 07 15 15 34 00]);
+calibrations{16}.from.gradient = 2.216464;
+calibrations{16}.from.offset = -4.427838;
+calibrations{16}.to.gradient = 2.235234;
+calibrations{16}.to.offset = -4.463728;
+
+calibrations{17}.channel = 39;
+calibrations{17}.startdatenum = datenum([2015 01 01 00 00 00]) ;
+calibrations{17}.stopdatenum = datenum([2015 07 06 17 10 00]) ;
+calibrations{17}.reason = 'Converting to TDMS system';
+calibrations{17}.dateadded = datenum([2015 07 15 15 34 00]);
+calibrations{17}.from.gradient = 2.220720;
+calibrations{17}.from.offset = -4.441985;
+calibrations{17}.to.gradient = 2.234743;
+calibrations{17}.to.offset = -4.464179;
+
+calibrations{18}.channel = 40;
+calibrations{18}.startdatenum = datenum([2015 01 01 00 00 00]) ;
+calibrations{18}.stopdatenum = datenum([2015 07 06 17 10 00]) ;
+calibrations{18}.reason = 'Converting to TDMS system';
+calibrations{18}.dateadded = datenum([2015 07 15 15 34 00]);
+calibrations{18}.from.gradient = 71.110300;
+calibrations{18}.from.offset = 97.311889;
+calibrations{18}.to.gradient = 70.936224;
+calibrations{18}.to.offset = 101.647652;
+
+calibrations{19}.channel = 41;
+calibrations{19}.startdatenum = datenum([2015 01 01 00 00 00]) ;
+calibrations{19}.stopdatenum = datenum([2015 07 06 17 10 00]) ;
+calibrations{19}.reason = 'Converting to TDMS system';
+calibrations{19}.dateadded = datenum([2015 07 15 15 34 00]);
+calibrations{19}.from.gradient = 70.894000;
+calibrations{19}.from.offset = 98.470000;
+calibrations{19}.to.gradient = 71.171079;
+calibrations{19}.to.offset = 99.480235;
+
+calibrations{20}.channel = 42;
+calibrations{20}.startdatenum = datenum([2015 01 01 00 00 00]) ;
+calibrations{20}.stopdatenum = datenum([2015 07 06 17 10 00]) ;
+calibrations{20}.reason = 'Converting to TDMS system';
+calibrations{20}.dateadded = datenum([2015 07 15 15 34 00]);
+calibrations{20}.from.gradient = 70.772400;
+calibrations{20}.from.offset = 102.540000;
+calibrations{20}.to.gradient = 70.961404;
+calibrations{20}.to.offset = 98.918776;
+
+calibrations{21}.channel = 43;
+calibrations{21}.startdatenum = datenum([2015 01 01 00 00 00]) ;
+calibrations{21}.stopdatenum = datenum([2015 07 06 17 10 00]) ;
+calibrations{21}.reason = 'Converting to TDMS system';
+calibrations{21}.dateadded = datenum([2015 07 15 15 34 00]);
+calibrations{21}.from.gradient = 70.995900;
+calibrations{21}.from.offset = 95.040000;
+calibrations{21}.to.gradient = 71.073782;
+calibrations{21}.to.offset = 94.886621;
+
+calibrations{22}.channel = 44;
+calibrations{22}.startdatenum = datenum([2015 01 01 00 00 00]) ;
+calibrations{22}.stopdatenum = datenum([2015 07 06 17 10 00]) ;
+calibrations{22}.reason = 'Converting to TDMS system';
+calibrations{22}.dateadded = datenum([2015 07 15 15 34 00]);
+calibrations{22}.from.gradient = 71.043000;
+calibrations{22}.from.offset = 98.100000;
+calibrations{22}.to.gradient = 71.029193;
+calibrations{22}.to.offset = 99.472130;
+
+calibrations{23}.channel = 63;
+calibrations{23}.startdatenum = datenum([2015 01 01 00 00 00]) ;
+calibrations{23}.stopdatenum = datenum([2015 07 06 17 10 00]) ;
+calibrations{23}.reason = 'Converting to TDMS system';
+calibrations{23}.dateadded = datenum([2015 07 15 15 34 00]);
+calibrations{23}.from.gradient = 51.995000;
+calibrations{23}.from.offset = 740.007000;
+calibrations{23}.to.gradient = 59.944750;
+calibrations{23}.to.offset = 759.658400;
+
